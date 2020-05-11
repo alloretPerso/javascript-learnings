40d6f3bf612ddbb58507860c4dfc15ceb6a66d41
alexandre.lloret <alloret.perso@gmail.com>
2020-05-11 08:47:47 +0100

    Work with asynchrone method

---
+++ async-programming-promises/src/creating.mjs
@@ -0,0 +1,43 @@
+import setText, { appendText } from "./results.mjs";
+
+export function timeout() {
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Timeout!");
+    }, 1500);
+  });
+  wait.then((text) => setText(text));
+}
+
+export function interval() {
+  let counter = 0;
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+  wait
+    .then((text) => setText(text))
+    .finally(() => appendText(` DOne ${counter}`));
+}
+
+export function clearIntervalChain() {
+  let counter = 0;
+  let interval;
+  const wait = new Promise((resolve) => {
+    interval = setInterval(() => {
+      resolve(`Timeout! ${++counter}`);
+    }, 1500);
+  });
+  wait.then((text) => setText(text)).finally(() => clearInterval(interval));
+}
+
+export function xhr() {
+    
+}
+
+export function allPromises() {}
+
+export function allSettled() {}
+
+export function race() {}
